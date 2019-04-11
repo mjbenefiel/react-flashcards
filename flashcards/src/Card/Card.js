c6980c11765177e3ed3736bbf100c73cedc3b708
mjbenefiel <michaeljbenefiel@gmail.com>
2019-04-11 14:18:11 -0700

    initial component

---
+++ flashcards/src/Card/Card.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./Card.css";
+
+const Card = props => (
+  <div className="card-container">
+    <div className="card">
+      <div className="front">
+
+      <div className="question">Question</div>
+      </div>
+
+      <div className="back">
+
+      <div className="Answer">Answer</div>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default Card
